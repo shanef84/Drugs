f39a42446deb6c7f145ed78b756a5d2f74881702
Shane Forbes <shane.forbes@student.gcd.ie>
2017-05-20 15:54:07 +0100

    Initial commit

---
+++ app/src/main/java/forbes/drugs/MainActivity.java
@@ -0,0 +1,183 @@
+package forbes.drugs;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+        implements  NavigationView.OnNavigationItemSelectedListener,
+                    AdapterView.OnItemClickListener {
+
+    private static final Class[] CLASSES = new Class[]{
+            GoogleSignInActivity.class,
+            TwitterLoginActivity.class,
+            EmailPasswordActivity.class,
+            AnonymousAuthActivity.class,
+            //FirebaseUIActivity.class,
+            //CustomAuthActivity.class
+    };
+
+    private static final int[] DESCRIPTION_IDS = new int[] {
+            R.string.desc_google_sign_in,
+            R.string.desc_twitter_login,
+            R.string.desc_emailpassword,
+            R.string.desc_anonymous_auth,
+            //R.string.desc_firebase_ui,
+           // R.string.desc_custom_auth,
+    };
+
+    private static final String TAG = "EmailPassword";
+
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set up ListView and Adapter
+        ListView listView = (ListView) findViewById(R.id.list_view);
+
+        MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_2, CLASSES);
+        adapter.setDescriptionIds(DESCRIPTION_IDS);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.med_link) {
+            // Handle the med action
+        } else if (id == R.id.search_link) {
+
+        } else if (id == R.id.reports_link) {
+
+        } else if (id == R.id.web_link) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.twitter_link) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Class clicked = CLASSES[position];
+        startActivity(new Intent(this, clicked));
+    }
+
+    public static class MyArrayAdapter extends ArrayAdapter<Class> {
+
+        private Context mContext;
+        private Class[] mClasses;
+        private int[] mDescriptionIds;
+
+        public MyArrayAdapter(Context context, int resource, Class[] objects) {
+            super(context, resource, objects);
+
+            mContext = context;
+            mClasses = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(android.R.layout.simple_list_item_2, null);
+            }
+
+            ((TextView) view.findViewById(android.R.id.text1)).setText(mClasses[position].getSimpleName());
+            ((TextView) view.findViewById(android.R.id.text2)).setText(mDescriptionIds[position]);
+
+            return view;
+        }
+
+        public void setDescriptionIds(int[] descriptionIds) {
+            mDescriptionIds = descriptionIds;
+        }
+    }
+}
